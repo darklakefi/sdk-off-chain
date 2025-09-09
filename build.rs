7bb6809a511c1ed2812ce39519aa10a3dadbba3d
Giovanni Colapinto <giovanni.colapinto@gmail.com>
2025-09-09 19:04:52 +0200

    fix: Add MIT headers

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+// (c) Darklake Labs PTE Ltd.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 use std::io::Result;
 
 fn main() -> Result<()> {
